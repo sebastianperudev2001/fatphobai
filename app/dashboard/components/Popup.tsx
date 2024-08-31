aef31b41c49ec9a5e723e7ea814f48440feb1453
Sebastian <sebastianchavarry2001@gmail.com>
2024-08-31 16:03:36 -0500

    feat: Icons + Fields dynamic fields for input data

--- app/dashboard/components/Popup.tsx
+++ app/dashboard/components/Popup.tsx
@@ -11,8 +11,63 @@ import {
   Input,
   Link,
 } from "@nextui-org/react";
-import { MailIcon } from "./Mailicon";
-import { LockIcon } from "./Lockicon";
+
+import { FaWeightScale } from "react-icons/fa6";
+import { FaHamburger } from "react-icons/fa";
+import { IoIosBody } from "react-icons/io";
+import { GiMuscleUp, GiEating, GiCakeSlice } from "react-icons/gi";
+import { RiBodyScanFill } from "react-icons/ri";
+
+const StyledIcon = ({ Icon }: { Icon: React.ElementType }) => {
+  const IconComponent = Icon; // Ensure Icon is treated as a component
+  return (
+    <IconComponent className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+  );
+};
+const valuesLogFields = [
+  {
+    label: "Weight",
+    placeholder: "Enter your weight (kg)",
+    icon: FaWeightScale,
+    isRequired: true,
+  },
+  {
+    label: "BMI",
+    placeholder: "Enter your BMI",
+    icon: IoIosBody,
+    isRequired: true,
+  },
+  {
+    label: "Body Fat",
+    placeholder: "Enter your body fat %",
+    icon: FaHamburger,
+    isRequired: true,
+  },
+  {
+    label: "Muscle Mass",
+    placeholder: "Enter your muscle mass %",
+    icon: GiMuscleUp,
+    isRequired: true,
+  },
+  {
+    label: "Resting Metabolism (RM)",
+    placeholder: "Enter your RM",
+    icon: GiEating,
+    isRequired: false,
+  },
+  {
+    label: "Body Age",
+    placeholder: "Enter your body age",
+    icon: RiBodyScanFill,
+    isRequired: false,
+  },
+  {
+    label: "Visceral Fat",
+    placeholder: "Enter your visceral fat",
+    icon: GiCakeSlice,
+    isRequired: false,
+  },
+];
 
 const Popup = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -26,27 +81,20 @@ const Popup = () => {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">
-                Daily Checkup
+              <ModalHeader className="flex flex-col gap-1 text-center ">
+                Daily Checkup 💪
               </ModalHeader>
               <ModalBody>
-                <Input
-                  endContent={
-                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Email"
-                  placeholder="Enter your email"
-                  variant="bordered"
-                />
-                <Input
-                  endContent={
-                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  variant="bordered"
-                />
+                {valuesLogFields.map((field, index) => (
+                  <Input
+                    key={index}
+                    endContent={field.icon && <StyledIcon Icon={field.icon} />}
+                    label={field.label}
+                    placeholder={field.placeholder}
+                    variant="bordered"
+                    isRequired={field.isRequired}
+                  />
+                ))}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="flat" onPress={onClose}>
